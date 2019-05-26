3587c265cc2e54034f71396afdeba995d39192e6
Leandro <leandrosuarez0@gmail.com>
2019-05-26 01:22:12 -0300

    Agregadas clases Loader, RawModel y Renderer, (renderizan vertices) y en el main hay un test.

--- src/com/engine/Tester/MainGameLoop.java
+++ src/com/engine/Tester/MainGameLoop.java
@@ -1,6 +1,9 @@
 package com.engine.Tester;
 
 import com.engine.render.DisplayManager;
+import com.engine.render.Loader;
+import com.engine.render.RawModel;
+import com.engine.render.Renderer;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -11,7 +14,26 @@ public class MainGameLoop {
     public static void main(String[] args){
 
         DisplayManager.createDisplay();
+
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f,-0.5f, 0f,
+                 0.5f,-0.5f, 0f,
+
+                 0.5f,-0.5f, 0f,
+                 0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f,
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while(!Display.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
+
             Display.update();
         }
 
