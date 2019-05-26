9d6d2a9a5414ff5e497f3982ca3d5b5b2ce55ad2
Leandro <leandrosuarez0@gmail.com>
2019-05-26 02:28:22 -0300

    Agregada union por indices entre vertices

--- src/com/engine/tester/MainGameLoop.java
+++ src/com/engine/tester/MainGameLoop.java
@@ -19,14 +19,16 @@ public class MainGameLoop {
         float[] vertices = {
                 -0.5f, 0.5f, 0f,
                 -0.5f,-0.5f, 0f,
-                 0.5f,-0.5f, 0f,
-
-                 0.5f,-0.5f, 0f,
-                 0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f,
+                0.5f,-0.5f, 0f,
+                0.5f, 0.5f, 0f,
         };
 
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices = {
+                0,1,3,
+                3,1,2
+        };
+
+        RawModel model = loader.loadToVAO(vertices,indices);
 
         while(!Display.isCloseRequested()) {
             renderer.prepare();
