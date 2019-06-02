ecc92466af8062f52aa34babe3b96cae125522b8
Leandro <leandrosuarez0@gmail.com>
2019-06-02 15:14:09 -0300

    MainLoop cambio.

--- src/com/engine/tester/MainLoop.java
+++ src/com/engine/tester/MainLoop.java
@@ -5,8 +5,10 @@ import com.engine.entities.Entity;
 import com.engine.entities.Light;
 import com.engine.models.RawModel;
 import com.engine.models.TexturedModel;
-import com.engine.render.*;
-import com.engine.shaders.StaticShader;
+import com.engine.render.DisplayManager;
+import com.engine.render.Loader;
+import com.engine.render.MasterRender;
+import com.engine.render.OBJLoader;
 import com.engine.textures.ModelTexture;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -19,15 +21,12 @@ public class MainLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-
         RawModel model = OBJLoader.loadObjModel("stall", loader);
-
         TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
 
         ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(0.3f);
-
 
         Entity entity = new Entity(staticModel, new Vector3f(0, -3.5f, -15f), 0, 0.1f, 0, 1);
 
@@ -39,14 +38,11 @@ public class MainLoop {
         while(!Display.isCloseRequested()) {
             entity.increasePosition(0, 0, 0);
             entity.increaseRotation(0, 0.4f, 0);
-
             renderer.processEntity(entity);
-
             camera.move();
             renderer.render(light, camera);
             DisplayManager.updatedisplay();
         }
-
         renderer.cleanUp();
         Display.destroy();
     }
